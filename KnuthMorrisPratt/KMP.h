60cc6e98badfade7f3bec40736ab444df4b2626b
Erik Messerli <emesserli@gmx.de>
2017-02-24 08:02:14 +0100

    readability fixes

--- KnuthMorrisPratt/KMP.h
+++ KnuthMorrisPratt/KMP.h
@@ -4,53 +4,49 @@
 #include <list>
 
 
-// Template Parameter 2) Type for collection of return values (defaults to vector, can be anything that supports emplace_back)  
+/*
+find_each
+arguments: Beginning and ending iterator to two (parts of) containers you want to pattern match on, optional: bool allow overlapping matches
+result: std::vector of iterators on the first container, each marking the beginning of a match
+time: linear in the length of the longer input
+*/
+
+//TODO: Template Parameter 2) Type for collection of return values (defaults to vector, can be anything that supports emplace_back)  
 
 namespace find_each {
 
-	//T some kind of forward iterator
+	
 	template<typename T> void MakePrefixFct(T s_begin, T s_end, std::vector<int>& prefix_fct_jump, std::vector<T>& prefix_fct) {
-		int count = 0;
-		for (T it = s_begin; it != s_end; ++it) {
-			count++;
-		}
+		auto count = std::distance(s_begin,s_end);
+
 		prefix_fct_jump.reserve(count);
 		prefix_fct.reserve(count);
 
 		prefix_fct_jump.push_back(0);
 		prefix_fct.push_back(s_begin);
 
-		T s_run = s_begin;
+		T s_run(s_begin);
 		s_run++;//Start iteration at second element
 
 		int k = 0; //Counts matching prefix
-		T k_run = s_begin; //P[k+1]
+		T k_run(s_begin); //P[k+1]
 
 		while (s_run != s_end) {
 			while (k > 0 && *(k_run) != *s_run) {
-				//cout << *k_run << " vs " << *s_run << " k=" << k << endl;
 				k_run = prefix_fct[k - 1];
 				k = prefix_fct_jump[k - 1];
-				//cout << *k_run << " vs " << *s_run << " k=" << k << endl;
 			}
 
 
 			if (*(k_run) == *s_run) {
 				k++;
-				/*if (k == 1) {
-					k_run = s_begin;
-				}
-				else { */
-				++k_run;
-				//}
+				k_run++;
 			}
 
-			//cout << *s_run << "" << k << " ";
 			prefix_fct_jump.emplace_back(k);
 			prefix_fct.emplace_back(k_run);
-			//cout << *prefix_fct[prefix_fct.size() - 1] << endl;
 
-			++s_run;
+			s_run++;
 		}
 	};
 
@@ -61,24 +57,25 @@ namespace find_each {
 		std::vector<int> prefix_fct_jump;
 		std::vector<TS> prefix_fct;
 		MakePrefixFct<TS>(s_begin, s_end, prefix_fct_jump, prefix_fct);
+		size_t prefix_fct_size = prefix_fct.size();
 
 		//List drop backlog is the element of the long string that was active m iterations ago,
 		//or l_end if we aren't at least m elements in
-		TL backlog = l_end;
+		TL backlog(l_end);
 
 
-		TL l_run = l_begin; //Iterator on long string
+		TL l_run(l_begin); //Iterator on long string
 		unsigned int i = 0; //pos on long string as int
 
-		TS s_run = s_begin; //pos on short string
-		int q = 0; //current match length (=pos on short string as int)
+		TS s_run(s_begin); //pos on short string
+		unsigned int q = 0; //current match length (=pos on short string as int)
 
 		while (l_run != l_end) {
 			//Keep a log of recent iterators to be able to output the beginnings of matches
-			if (i == prefix_fct.size() - 1) {
+			if (i == prefix_fct_size - 1) {
 				backlog = l_begin;
 			}
-			if (i >= prefix_fct.size()) {
+			if (i >= prefix_fct_size) {
 				backlog++;
 			}
 
@@ -93,20 +90,19 @@ namespace find_each {
 
 			//Count up length of match
 			if (*s_run == *l_run) {
-				++s_run;
+				s_run++;
 				q++;
 			}
 
 			//On a match push the first element of the backlog as a result, reduce to the next highest matching prefix
 			//We allow matches to overlap!
 			if (s_run == s_end) {
-				//cout << "match! "<< (i - prefix_fct.size() + 1) <<endl;
 				//matches.emplace_back(backlog.front());
 				matches.push_back(backlog);
 
 				if (overlap_matches) {
-					s_run = prefix_fct[prefix_fct.size() - 1]; //If we allow overlapping matches we might not need to go all the way back.
-					q = prefix_fct_jump[prefix_fct_jump.size() - 1];
+					s_run = prefix_fct[prefix_fct_size - 1]; //If we allow overlapping matches we might not need to go all the way back.
+					q = prefix_fct_jump[prefix_fct_size - 1];
 				}
 				else {
 					s_run = s_begin; //Complete reset if we don't want overlapping matches
@@ -116,7 +112,7 @@ namespace find_each {
 
 
 			i++;
-			++l_run;
+			l_run++;
 		}
 
 
--- KnuthMorrisPratt/Main.cpp
+++ KnuthMorrisPratt/Main.cpp
@@ -14,40 +14,16 @@ int main() {
 
 	string a = "aababab";
 	string b = "abab";
-	/*
-	vector<float>  a;
-	for (int i = 0; i < 50; i++) {
-		a.push_back( i%5);
-	}
-	vector<float> b;
-	for (int i = 0; i < 10; i++) {
-		b.push_back( i % 5 );
-	}
-	*/
+
+
 	int i = 0;
 
 	auto res = find_each::find_each(a.cbegin(),a.cend(),b.cbegin(),b.cend(),true); //<std::vector<int>::iterator, std::vector<float>::iterator,std::vector<int>>
-	cout << "#" << res.size()<<endl;
+	cout << '#' << res.size()<<endl;
 	for (int i = 0; i < res.size(); i++) {
 		cout << *res[i] << endl;
 	}
 
-/*
-  i = 1;
-  vector<vector<float>::const_iterator> test;
-  vector<float>::const_iterator al = a.cbegin();
-  while (al != a.cend()) {
-	  auto res2 = search(al, a.cend(), b.cbegin(), b.cend());
-	  
-	  if (res2 == a.cend()) { break; }
-	  else { test.push_back(res2); al = res2; al++; }
-  }
-
-
-	 i = 2;
-	 */
-	 //Test output to see if we really have the proper spots
-	 //cout << res.size() << "|" << test.size() << endl; //  << " | " << res2.size();
 
 
 	system("pause");
